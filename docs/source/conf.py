f842d57c3b62d9431e7b40c1437a72b904ef6224
Özgür Cakir <cakir@netmodule.com>
2023-10-21 15:22:16 +0200

    commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Lilypad'
+copyright = '2023, Cakir'
+author = 'Cakir'
 
 release = '0.1'
 version = '0.1.0'
